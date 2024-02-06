712c2f16c7b77e3b91ea36631aa389cc4282b840
RuntoUfrom <126852315+RuntoUfrom@users.noreply.github.com>
2024-02-06 16:35:10 +0900

    Title: 살 수 있는 물건, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240206/살 수 있는 물건/things-able-to-buy.cpp
+++ 240206/살 수 있는 물건/things-able-to-buy.cpp
@@ -3,11 +3,14 @@ using namespace std;
 int main() {
     int m;
     cin>>m;
-    if(m>=3000){
-        cout<<"book";
-    }
-    else if(m>=1000&&m<3000){
+    if(m>=1000&&m<3000){
         cout<<"mask";
+    }
+    else if(m>=3000){
+         cout<<"book";
+    }
+    else{
+        cout<<"no"
     }
     return 0;
 }
