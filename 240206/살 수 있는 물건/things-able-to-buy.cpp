4decd4c28947eff9eccdbdb86dfcf66722430875
RuntoUfrom <126852315+RuntoUfrom@users.noreply.github.com>
2024-02-06 16:38:16 +0900

    Title: 살 수 있는 물건, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240206/살 수 있는 물건/things-able-to-buy.cpp
+++ 240206/살 수 있는 물건/things-able-to-buy.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int m;
-    cin>>m;
-    if(m>=1000&&m<3000){
-        cout<<"mask";
-    }
-    else if(m>=3000){
-         cout<<"book";
-    }
-    else{
-        cout<<"no"
-    }
-    return 0;
+	int m;
+	cin >> m;
+	if (m >= 1000) {
+		if (m >= 3000) {
+			cout << "book";
+		}
+		else {
+			cout << "mask";
+		}
+	}
+	else {
+		cout << "no";
+	}
 }
