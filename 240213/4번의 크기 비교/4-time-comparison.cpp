8f4ee5ed4cbd8e79410058f756d1cd2655bf1094
RuntoUfrom <126852315+RuntoUfrom@users.noreply.github.com>
2024-02-13 16:08:15 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240213/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin>>a;
+    int b,c,d,e;
+    cin>>b>>c>>d>>e;
+    if(a>b){
+        cout<<1<<endl;
+    }
+    else{
+        cout<<0<<endl;
+    }
+    if(a>c){
+        cout<<1<<endl;
+    }
+    else{
+        cout<<0<<endl;
+    }
+    if(a>d){
+        cout<<1<<endl;
+    }
+    else{
+        cout<<0<<endl;
+    }
+    if(a>e){
+        cout<<1<<endl;
+    }
+    else{
+        cout<<0<<endl;
+    }
+    return 0;
+}
