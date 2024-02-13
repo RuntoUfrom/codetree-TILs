2ff5c33aee106e7f5eee05989d17976364972972
RuntoUfrom <126852315+RuntoUfrom@users.noreply.github.com>
2024-02-13 16:24:14 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240213/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    int mid;
+    if(a>=b&&a<=c){
+        mid=a;
+    }
+    else if(b>=a&&b<=c){
+        mid = b;
+    }
+    else{
+        mid=c;
+    }
+    cout<<mid;
+    return 0;
+}
