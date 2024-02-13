262c702a74c703ad9d7bcf9f69cab88eeddb822e
RuntoUfrom <126852315+RuntoUfrom@users.noreply.github.com>
2024-02-13 21:57:53 +0900

    Title: 짝수만 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240213/짝수만 출력 2/output-only-even-2.cpp
+++ 240213/짝수만 출력 2/output-only-even-2.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
-using namespace std;
-int main() {
-    int b, a;
-    cin >> b >> a;
 
-    while (b >= a) {
-        if (b% 2 == 0) {
-            cout << b << " ";
-        }
-        b--;
-    }
+using namespace std;
+
+int main() {
+    // 변수 선언
+    int b, a, i;
+
+	// 입력
+	cin >> b >> a;
+	
+	i = b;
+    
+    // 출력
+	while(i >= a) {
+		cout << i << " ";
+		i -= 2;
+	}
+
     return 0;
 }
