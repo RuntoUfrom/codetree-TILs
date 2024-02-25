beaea3580be423495229b58f8829945fb4c9ebf1
RuntoUfrom <126852315+RuntoUfrom@users.noreply.github.com>
2024-02-25 21:02:22 +0900

    Title: 전항의 두 배, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240225/전항의 두 배/twice-the-previous.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[100];
+    for(int i = 0; i<2;i++){
+        cin>>arr[i];
+    }
+    for(int i = 2;i<10; i++){
+        arr[i] = arr[i-1]+2*arr[i-2];
+    }
+    for(int i = 0; i<10;i++){
+        cout<< arr[i]<<" ";
+    }
+    return 0;
+}
