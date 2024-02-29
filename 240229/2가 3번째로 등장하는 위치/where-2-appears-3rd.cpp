4daeb6d20a2aa9e18cb7000b0d5ffd28594b39a1
RuntoUfrom <126852315+RuntoUfrom@users.noreply.github.com>
2024-02-29 19:25:42 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240229/2가 3번째로 등장하는 위치/where-2-appears-3rd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int cnt = 0;
+    int tmp;
+    for(int i = 0; i<n; i++){
+        cin>>tmp;
+        if(tmp==2){
+            cnt++;
+            if(cnt==3){
+                cout<<i+1;
+            }
+        }
+    }
+    return 0;
+}
