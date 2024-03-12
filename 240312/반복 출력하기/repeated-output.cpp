b0a1cef3d261363a0374549d55af3459982263fb
RuntoUfrom <126852315+RuntoUfrom@users.noreply.github.com>
2024-03-12 12:31:00 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240312/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void output(int n){
+    for(int i = 0; i<n; i++){
+        cout<<"12345^&*()_"<<endl;
+    }
+}
+int main() {
+    int n;
+    cin>>n;
+   output(n);
+    return 0;
+}
