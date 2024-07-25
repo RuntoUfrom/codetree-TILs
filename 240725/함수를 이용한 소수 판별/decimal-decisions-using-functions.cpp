d4e33ac0a5674e7e5c7fabcd0f9291a69f150aac
RuntoUfrom <126852315+RuntoUfrom@users.noreply.github.com>
2024-07-25 14:36:00 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240725/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 240725/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 bool isPrime(int n) {
     bool is_prime = true;
+    if(n==1){
+        is_prime = false;
+    }
     for (int i = 2; i < n; i++) {
         if (n % i == 0) {
             is_prime = false;
