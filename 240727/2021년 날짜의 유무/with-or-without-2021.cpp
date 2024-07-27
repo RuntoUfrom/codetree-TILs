ecde75d565cf2dcf3997b78f8461d13b5299e01c
RuntoUfrom <126852315+RuntoUfrom@users.noreply.github.com>
2024-07-27 14:32:42 +0900

    Title: 2021년 날짜의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240727/2021년 날짜의 유무/with-or-without-2021.cpp
+++ 240727/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -2,59 +2,60 @@
 using namespace std;
 
 bool isDay(int m, int d){
+    bool tmp = false;
     switch(m){
         case 1: 
             if(d<=31)
-            return true;
+            tmp = true;
             break;
         case 2: 
             if(d<=28)
-            return true;
+            tmp = true;
             break;
         case 3: 
             if(d<=31)
-            return true;
+            tmp = true;
             break;
         case 4: 
             if(d<=30)
-            return true;
+            tmp = true;
             break;
         case 5: 
             if(d<=31)
-            return true;
+            tmp = true;
             break;
         case 6: 
             if(d<=30)
-            return true;
+           tmp = true;
             break;
         case 7: 
             if(d<=31)
-            return true;
+            tmp = true;
             break;
         case 8: 
             if(d<=31)
-            return true;
+           tmp = true;
             break;
         case 9: 
             if(d<=30)
-            return true;
+            tmp = true;
             break;
         case 10: 
             if(d<=31)
-            return true;
+           tmp = true;
             break;
         case 11: 
             if(d<=30)
-            return true;
+            tmp = true;
             break;
         case 12: 
             if(d<=31)
-            return true;
+            tmp = true;
             break;
         default :
-            return false;
             break;
     }
+    return tmp;
 }
 
 int main() {
