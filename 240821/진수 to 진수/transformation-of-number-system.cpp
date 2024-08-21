a6b36f172340ab1b5feb2df96f1e32a07ed71046
RuntoUfrom <126852315+RuntoUfrom@users.noreply.github.com>
2024-08-21 12:39:09 +0900

    Title: 진수 to 진수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240821/진수 to 진수/transformation-of-number-system.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+int toDec(int a, string num){
+  int tmp = 0;
+  for(int i = 0; i<(int)num.length(); i++){
+       tmp = tmp*a+(num[i]-'0');
+    }
+    return tmp;
+}
+void toB(int b,int num){
+    vector<int> v;
+    while(true){
+        if(num<b){
+            v.push_back(num);
+            break;
+        }
+        v.push_back(num%b);
+        num/=b;
+    }
+    for(int i =0; i<v.size(); i++){
+        cout<<v[i];
+    }
+}
+
+int main() {
+    int a,b;
+    string n;
+    cin>>a>>b>>n;
+    toB(b,toDec(a,n));
+    
+    return 0;
+}
