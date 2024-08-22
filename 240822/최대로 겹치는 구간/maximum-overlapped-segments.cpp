bf19ca612c07d26574317ef497a7f5613f668699
RuntoUfrom <126852315+RuntoUfrom@users.noreply.github.com>
2024-08-22 20:45:18 +0900

    Title: 최대로 겹치는 구간, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240822/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 240822/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include<vector>
-#include<utility>
+//#include<utility>
 #include<cmath>
 #include<algorithm>
 #include<functional>
@@ -11,23 +11,18 @@ void coloring(int x1,int x2,vector<int> &v){
         v[i] = v[i]+1;
     }
 }
-pair<int,int> offset(int x1,int x2){
-    int tmp = abs(x1);
-    x1+=tmp;
-    x2+=tmp;
-    pair<int,int> p = make_pair(x1,x2);
-    return p;
-}
 int main() {
     int n;
     cin>>n;
-    vector<int> v(n,0);
+    vector<int> v(200,0);
     for(int i = 0; i<n; i++){
         int x1,x2;
         cin>>x1>>x2;
         if(x1<0||x2<0){
-           x1= offset(x1,x2).first;
-           x2 = offset(x1,x2).second;
+           int tmp = abs(x1);
+            x1+=tmp;
+            x2+=tmp;
+           
         }
         coloring(x1,x2-1,v);
     }
